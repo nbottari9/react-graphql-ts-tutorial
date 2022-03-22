641e310a105074050af644d73a4f05038b5a1e2f
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2022-03-21 23:11:58 -0400

    beginning of video to start of frontend

---
+++ lireddit-web/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+export const Register: React.FC<registerProps> = ({}) => {
+        return <div>register page</div>;
+        
+}
+
+export default Register
